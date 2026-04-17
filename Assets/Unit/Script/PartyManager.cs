e994fe7dd8104810674e6588579172ff737d2854
CHOIMINJUN(최민준) <s24071@gsm.hs.kr>
2026-04-17 11:49:32 +0900

    party base
    party base

--- Assets/Unit/Script/PartyManager.cs
+++ Assets/Unit/Script/PartyManager.cs
@@ -1,8 +1,10 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PartyManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    List<GameObject> partyMembers = new List<GameObject>();
     void Start()
     {
         
@@ -12,5 +14,31 @@ public class PartyManager : MonoBehaviour
     void Update()
     {
         
+    }
+    public void addParty()
+    {
+        if(partyMembers.Count < 4)
+        {
+            GameObject newMember = new GameObject("PartyMember" + (partyMembers.Count + 1));
+            partyMembers.Add(newMember);
+            Debug.Log("Added: " + newMember.name);
+        }
+        else
+        {
+            Debug.Log("Party is full!");
+        }
+    }
+    public void removeParty()
+    {
+        if(partyMembers.Count > 0)
+        {
+            GameObject removedMember = partyMembers[partyMembers.Count - 1];
+            partyMembers.RemoveAt(partyMembers.Count - 1);
+            Debug.Log("Removed: " + removedMember.name);
+        }
+        else
+        {
+            Debug.Log("No members to remove!");
+        }
     }
 }
